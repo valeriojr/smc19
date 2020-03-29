1c8a89b1e2e35319b314c7fa9954b3c8be1fdc8d
Valério Nogueira Rodrigues Júnior <vnrj@ic.ufal.br>
2020-03-28 23:17:31 -0300

    Deploy

--- smc19/settings.py
+++ smc19/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 if DEBUG:
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR,  'static'),
     )
     STATIC_URL = '/static/'
 else:
